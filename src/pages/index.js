b41008fdf3ae23900ff9d7899d9c7adc301a48a2
trust <trust4u@hotmail.it>
2021-02-02 12:11:01 +0100

    -

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Typography from '../content/base/typography';
 export default function index() {
   return (
     <div>
-      <Typography variant="primary">this is good</Typography>
+      <Typography variant="primary" color="success">this is good</Typography>
     </div>
   )
 }
