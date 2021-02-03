da7b121585f85d52177f1e3bb2df7ba889f2b489
trust <trust4u@hotmail.it>
2021-02-04 00:19:36 +0100

    .

--- src/content/components/button/index.jsx
+++ src/content/components/button/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Btn from "./style";
-export default function Button({ children, variant, onClick }) {
+export default function Button({ children, variant, onClick, className }) {
   return (
-    <Btn variant={variant} onClick={onClick}>
+    <Btn variant={variant} onClick={onClick} className={className}>
       {children}
     </Btn>
   );
